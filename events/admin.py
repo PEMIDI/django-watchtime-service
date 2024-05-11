7ab0552f8cf30f132d7898763e5b9287e2c9274e
pemidi <rashidi.peyman@gmail.com>
2024-05-11 06:10:25 +0330

    register models in admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CustomUser, Movie, WatchTime
 
-# Register your models here.
+
+class WatchTimeInline(admin.TabularInline):
+    model = WatchTime
+    extra = 1
+
+
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [WatchTimeInline]
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
