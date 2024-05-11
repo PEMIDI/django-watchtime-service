c0f43e0a8eb24b6f1bc3cf8c5cad48d169d1e61e
pemidi <rashidi.peyman@gmail.com>
2024-05-11 06:08:26 +0330

    add properties, change User model name to Custom User, changed User model to avoid overriding of django User

--- events/models.py
+++ events/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.db.models import Sum
 
 
-class User(models.Model):
+class CustomUser(models.Model):
     username = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.username
+
+    def total_watch_time(self):
+        total_watch_time = self.objects.all().annotate(total_watch_time=Sum('watch_time'))
+        return total_watch_time
 
 
 class Movie(models.Model):
@@ -17,9 +22,15 @@ class Movie(models.Model):
 
 
 class WatchTime(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="watch_time")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='watch_time')
     total_watch_time = models.PositiveIntegerField(default=0, editable=False)
 
     def __str__(self):
         return f"{self.user} watched movie {self.movie} at {self.total_watch_time}"
+
+    def update_watch_time(self, new_watch_time):
+        self.total_watch_time += new_watch_time
+        self.save()
+        return self.total_watch_time
+
