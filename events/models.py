395ba864bd93f1989322887d30b6dec9471ee9d0
pemidi <rashidi.peyman@gmail.com>
2024-05-11 05:45:55 +0330

    add User, Movie and WatchTime models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class WatchTime(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    total_watch_time = models.PositiveIntegerField(default=0, editable=False)
+
+    def __str__(self):
+        return f"{self.user} watched movie {self.movie} at {self.total_watch_time}"
