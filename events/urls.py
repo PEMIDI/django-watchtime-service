1ae7f072559dda3f9642e9127fd278b47dda60f8
pemidi <rashidi.peyman@gmail.com>
2024-05-12 03:03:00 +0330

    add endpoints to handle total watch time for movie and user

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from events import views
 
 urlpatterns = [
     path('', views.EventAPI.as_view(), name='event-list'),
+    path('users/<str:username>/total-watch-time/', views.UserTotalWatchTimeAPI.as_view(), name='user-watch-time'),
+    path('movies/<int:pk>/total-watch-time/', views.MovieTotalWatchTimeAPI.as_view(), name='movie-watch-time')
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
 import logging
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from events.models import Movie, CustomUser
+from events.serializers import MovieTotalWatchTimeSerializer, UserTotalWatchTimeSerializer
 from rabbitmq import rabbitmq_client
 
 
@@ -14,3 +16,14 @@ class EventAPI(APIView):
         rabbitmq_client.publish(request.data)
 
         return Response("ok", status=status.HTTP_200_OK)
+
+
+class UserTotalWatchTimeAPI(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserTotalWatchTimeSerializer
+    lookup_field = "username"
+
+
+class MovieTotalWatchTimeAPI(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieTotalWatchTimeSerializer
