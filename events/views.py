1d533f5a8741dd1db3284560a552c04cef87aed6
pemidi <rashidi.peyman@gmail.com>
2024-05-11 05:47:33 +0330

    add EventAPI to handle request from external service

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rabbitmq import rabbitmq_client
+
+
+class EventAPI(APIView):
+
+    def post(self, request):
+        logging.info(msg=f"received data from request: {request.data}")
+        rabbitmq_client.publish(request.data)
+
+        return Response("ok", status=status.HTTP_200_OK)
