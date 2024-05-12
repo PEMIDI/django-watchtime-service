b113e4e95b3a8c34f3320d1d6db6225f289f103c
pemidi <rashidi.peyman@gmail.com>
2024-05-12 06:28:19 +0330

    document the ```serializer.py```

--- events/serializers.py
+++ events/serializers.py
@@ -5,12 +5,29 @@ from events.models import Movie, CustomUser
 
 
 class UserTotalWatchTimeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for CustomUser model to retrieve total watch time 🕰️
+
+    Attributes:
+    - username (str): Username of the user
+    - total_watch_time (int): Total watch time of the user in seconds
+    """
+
     class Meta:
-        model = CustomUser
+        model = CustomUser # 👥
         fields = ['username', 'total_watch_time']
 
 
 class MovieTotalWatchTimeSerializer(ModelSerializer):
+    """
+    Serializer for Movie model to retrieve total watch time 🎥
+
+    Attributes:
+    - title (str): Title of the movie
+    - slug (str): Slug of the movie
+    - total_watch_time (int): Total watch time of the movie in seconds
+    """
+
     class Meta:
-        model = Movie
+        model = Movie # 🎬
         fields = ['title', 'slug', 'total_watch_time']
